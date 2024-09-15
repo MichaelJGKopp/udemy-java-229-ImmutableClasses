5528e987c3ae277433c3cdb9f465d2a8363fc328
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-15 14:34:19 +0200

    add MainImmutable testing immutability of PersonImmutable

---
+++ src/dev/external/MainImmutable.java
@@ -0,0 +1,26 @@
+package dev.external;
+
+import dev.lpa.PersonImmutable;
+
+public class MainImmutable {
+
+  public static void main(String[] args) {
+
+    PersonImmutable jane = new PersonImmutable("Jane", "01/01/1930");
+    PersonImmutable jim = new PersonImmutable("Jim", "02/02/1932");
+    PersonImmutable joe = new PersonImmutable("Joe", "03/03/1934");
+
+    PersonImmutable[] johnKids = {jane, jim, joe};
+    PersonImmutable john = new PersonImmutable("John", "04/04/1900", johnKids);
+
+    System.out.println(john);
+
+    PersonImmutable[] kids = john.getKids();
+    kids[0] = jim;
+    kids[1] = new PersonImmutable("Ann", "04/04/1936");
+    System.out.println(john);
+
+    johnKids[0] = new PersonImmutable("Ann", "04/04/1936");
+    System.out.println(john);
+  }
+}
