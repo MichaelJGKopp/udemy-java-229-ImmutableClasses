ab1ad7a0a94e18e2099bae64bc3ef15b2c6d388e
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-15 12:10:44 +0200

    add John and Family example in class Main

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,4 +1,24 @@
 package dev.lpa;
 
 public class Main {
+
+  public static void main(String[] args) {
+
+    Person jane = new Person();
+    jane.setName("Jane");
+    Person jim = new Person();
+    jim.setName("Jim");
+    Person joe = new Person();
+    joe.setName("Joe");
+    Person john = new Person();
+    john.setName("John");
+    john.setDob("05/05/1900");
+    john.setKids(new Person[] {jane, jim, joe});
+    System.out.println(john);
+
+    john.setName("Jacob");
+    john.setKids(new Person[]{new Person(), new Person()});
+    System.out.println(john);
+
+  }
 }
